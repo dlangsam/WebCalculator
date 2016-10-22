2eff4c8435bbf6eb8ecf28baf22d17bfe285ec68
Devorah Langsam <devorah@langsam.org>
2016-10-22 09:31:38 -0400

    Update display value when digit is touched

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 $(document).on('ready', function(){
-
+	var userInMiddleOfTyping = false; 
 	$('.js-digit').on('click', function(){
-		console.log("Digit clicked");
+		var digit = $(this).text();
+		console.log(digit + " clicked");
+		if(userInMiddleOfTyping){
+             var textCurrentlyInDisplay = $('.js-display').val();
+             if(textCurrentlyInDisplay.includes(".") && digit === "." ){return;}
+            $('.js-display').val(textCurrentlyInDisplay + digit);
+        }else{
+            if (digit === "."){
+                 $('.js-display').val("0.");
+            }else{
+                $('.js-display').val(digit); 
+            }
+        }
+        //print("Change typing to true")
+        userInMiddleOfTyping = true;
+
 	});
 
 
